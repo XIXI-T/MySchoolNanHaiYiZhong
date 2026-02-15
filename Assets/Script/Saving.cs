1253ea208cf3e7911fe69b3e7d59d9f7831e4035
unknown <skymmh01@163.com>
2026-02-15 23:50:55 +0800

    小改

---
+++ Assets/Script/Saving.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saving : MonoBehaviour
+{
+    public class SaveContent//存储内容
+    {
+        public string SceneName;//场景名称
+        public Vector2 PlayerPosition;//主角坐标
+        
+    }
+    public void SaveGame()
+    {
+        
+    }
+}
